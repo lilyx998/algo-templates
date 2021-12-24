5589f705f54d2d925f40c6510bc009a678ed26ea
lilyang <lilyang@umich.edu>
2021-12-24 12:28:23 -0500

    Added DIRS in c++

---
+++ Misc. /DIRS.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+// 4 directions
+int dirs[4][2]= {{-1,0},{1,0},{0,-1},{0,1}};
+
+
+// 8 directions
+int dirs[8][2] = {{-1,-1},{-1,1},{1,-1},{1,1},{-1,0},{1,0},{0,-1},{0,1}};
