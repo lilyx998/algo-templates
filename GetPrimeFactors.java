4a04022427223451475118cce2cfc069f1c94abd
lilyang <lilyang@umich.edu>
2022-02-25 12:03:57 -0500

    minor changes

--- GetPrimeFactors.java
+++ GetPrimeFactors.java
@@ -2,7 +2,7 @@ List<Integer> getPrimeFactors(int n){
   List<Integer> res = new ArrayList<>(); 
   for(int factor = 2; factor * factor <= n; factor++){
     while(n % factor == 0){
-      result.add(factor); 
+      res.add(factor); 
       n /= factor; 
     }
   }
